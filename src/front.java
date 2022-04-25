faed6cfeaaaa5aa5fd14e543f113a223e8a7a347
Paolo3100 <paolo31@live.com>
2022-04-25 15:36:29 +0100

    Final one before AI

--- src/front.java
+++ src/front.java
@@ -18,7 +18,10 @@ public class front extends Thread{
     JButton button = new JButton();
     MyButton [] arrayLabels = new MyButton[64];
     String myText = "";
-    boolean validation;
+    boolean validation1;
+    boolean validation2;
+    static front first = new front("Reversi - white player");
+    static front second = new front("Reversi - black player");
     front(String title){
         panel.setLayout( new GridLayout(8,8) ); // Setting the number of boxes
         visual.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Program should not run in background after exit
@@ -59,58 +62,54 @@ public class front extends Thread{
         visual.setSize(800,800);
         visual.setVisible(true);
     }
-    public void place1(front first,front second,int finalI1,one check, two check2){
+    public void place1(front first, int finalI1, one check){
         if (first.l1.getText() != " White player - click to put piece"){
-            JOptionPane.showMessageDialog(first.visual,"Await your turn: Double click to close");
+            JOptionPane.showMessageDialog(first.visual,"Await your turn");
         }
         if (first.l1.getText() == " White player - click to put piece"&& !first.arrayLabels[finalI1].isClicked) {
             System.out.println("This Version");
             // Checking to see if any more moves can be made:
             for (int val = 0; val < 64; val++) {
                 if(first.arrayLabels[val].col==Color.WHITE){
-                    first.validation = check.validation(first.arrayLabels, val);
-                    if (first.validation == true) {
-                        System.out.println("First: " + first.validation );
+                    validation1 = check.validation(first.arrayLabels, val);
+                    if (validation1 == true) {
                         break;
                     }
                     else{
-                        System.out.println("Keeps going");
                     }
                 }
             }
             // Checking to see if any moves can be made: FOR 2nd player
+            two check2 = new two();
             for (int val = 0; val < 64; val++) {
                 if(second.arrayLabels[val].col==Color.BLACK){
-                    second.validation = check2.validation(second.arrayLabels, val);
-                    if (second.validation == true) {
-                        System.out.println("Second: " + second.validation );
+                    validation2 = check2.validation(second.arrayLabels, val);
+                    if (validation2 == true) {
                         break;
                     }
                 }
             }
             // if no moves left for first but some left for right, pass to black
-            System.out.println("White Switching: "+first.validation +" "+second.validation);
-            if (!first.validation&&second.validation){
+            System.out.println("White Switching: "+validation1 +" "+validation2);
+            if (!validation1&&validation2){
                 first.l1.setText(" White player - not your turn");
                 second.l1.setText(" Black player - click to put piece");
 
             }
             // If they can, then check if pieces can be captured
-            if (first.validation) {
+            if (validation1) {
                 ArrayList<Integer> arr = check.manipulate(first.arrayLabels, finalI1);
                 // There are elements to capture
                 if (arr.size() != 0) {
-                    arrayLabels[finalI1].isClicked = true;
-                    arrayLabels[finalI1].col = Color.WHITE;
-                    arrayLabels[finalI1].repaint();
+                    first.arrayLabels[finalI1].isClicked = true;
+                    first.arrayLabels[finalI1].col = Color.WHITE;
+                    first.arrayLabels[finalI1].repaint();
 
                     for (int xi = 0; xi < arr.size(); xi++) {
-                        arrayLabels[arr.get(xi)].isClicked = true;
-                        arrayLabels[arr.get(xi)].col = Color.WHITE;
-                        arrayLabels[arr.get(xi)].repaint();
+                        first.arrayLabels[arr.get(xi)].isClicked = true;
+                        first.arrayLabels[arr.get(xi)].col = Color.WHITE;
+                        first.arrayLabels[arr.get(xi)].repaint();
                     }
-                    System.out.println("Test");
-
                     // Switching control to other player
                     Thread t2 = new Thread(new Runnable() {
                         @Override
@@ -134,25 +133,25 @@ public class front extends Thread{
             }
             //System.out.println("White: "+first.validation+" Black: "+second.validation);
             // Checking to see if any more moves can be made:
-            first.validation = false;
-            second.validation = false;
+            validation1 = false;
+            validation2 = false;
             for (int val = 0; val < 64; val++) {
                 if(first.arrayLabels[val].col==Color.WHITE){
-                    first.validation = check.validation(first.arrayLabels, val);
-                    if (first.validation) {
+                    validation1 = check.validation(first.arrayLabels, val);
+                    if (validation1) {
                         break;
                     }
                 }
             }
             for (int val = 0; val < 64; val++) {
                 if(second.arrayLabels[val].col==Color.BLACK){
-                    second.validation = check2.validation(second.arrayLabels, val);
-                    if (second.validation) {
+                    validation2 = check2.validation(second.arrayLabels, val);
+                    if (validation2) {
                         break;
                     }
                 }
             }
-            if (!first.validation&&!second.validation){
+            if (!validation1&&!validation2){
                 int white = 0;
                 int black = 0;
                 for (int i = 0;i<64;i++){
@@ -170,23 +169,23 @@ public class front extends Thread{
                 if(white==black){
                     JOptionPane.showMessageDialog(second.visual,"Draw: "+black+":"+white);
                 }
-                System.out.println("End White");
                 System.exit(0);
             }
 
         }
     }
-    public void place2(front first,front second,int finalI1,one check, two check2){
+    public void place2(front second,int finalI1,two check2){
         if (second.l1.getText() != " Black player - click to put piece"){
-            JOptionPane.showMessageDialog(second.visual,"Await your turn: Double click to close");
+            JOptionPane.showMessageDialog(second.visual,"Await your turn");
 
         }
         if(second.l1.getText() == " Black player - click to put piece" && !second.arrayLabels[finalI1].isClicked ){
             // Checking to see if any more moves can be made:
+            one check = new one();
             for (int val = 0; val < 64; val++) {
                 if(first.arrayLabels[val].col==Color.WHITE){
-                    first.validation = check.validation(first.arrayLabels, val);
-                    if (first.validation) {
+                    validation1 = check.validation(first.arrayLabels, val);
+                    if (validation1) {
                         break;
                     }
                 }
@@ -194,23 +193,23 @@ public class front extends Thread{
             // Checking to see if any moves can be made: FOR second player
             for (int val = 0; val < 64; val++) {
                 if(second.arrayLabels[val].col==Color.BLACK){
-                    second.validation = check2.validation(second.arrayLabels, val);
-                    if (second.validation) {
+                    validation2 = check2.validation(second.arrayLabels, val);
+                    if (validation2) {
                         break;
                     }
                 }
             }
-            if(second.validation){
+            if(validation2){
                 ArrayList<Integer> arr = check2.manipulate(second.arrayLabels, finalI1);
                 // There are elements to capture
                 if(arr.size() != 0) {
-                    arrayLabels[finalI1].isClicked = true;
-                    arrayLabels[finalI1].col = Color.BLACK;
-                    arrayLabels[finalI1].repaint();
+                    second.arrayLabels[finalI1].isClicked = true;
+                    second.arrayLabels[finalI1].col = Color.BLACK;
+                    second.arrayLabels[finalI1].repaint();
                     for (int xi = 0; xi < arr.size(); xi++) {
-                        arrayLabels[arr.get(xi)].isClicked = true;
-                        arrayLabels[arr.get(xi)].col = Color.BLACK;
-                        arrayLabels[arr.get(xi)].repaint();
+                        second.arrayLabels[arr.get(xi)].isClicked = true;
+                        second.arrayLabels[arr.get(xi)].col = Color.BLACK;
+                        second.arrayLabels[arr.get(xi)].repaint();
                     }
                     // Switching control to other player
                     // Switching control to other player
@@ -233,8 +232,7 @@ public class front extends Thread{
                 count++;
             }
             // if no moves left for second but some left for first
-            if (first.validation&&!second.validation){
-                System.out.println("Black Switching");
+            if (validation1&&!validation2){
                 Thread t2 = new Thread(new Runnable() {
                     @Override
                     public void run() {
@@ -247,12 +245,12 @@ public class front extends Thread{
             }
             //System.out.println("White: "+first.validation+" Black: "+second.validation);
             // Checking to see if any moves can be made: FOR second player
-            first.validation = false;
-            second.validation = false;
+            validation1 = false;
+            validation2 = false;
             for (int val = 0; val < 64; val++) {
                 if(second.arrayLabels[val].col==Color.BLACK){
-                    second.validation = check2.validation(second.arrayLabels, val);
-                    if (second.validation) {
+                    validation2 = check2.validation(second.arrayLabels, val);
+                    if (validation2) {
                         break;
                     }
                 }
@@ -260,13 +258,13 @@ public class front extends Thread{
             // Checking to see if any more moves can be made:
             for (int val = 0; val < 64; val++) {
                 if(first.arrayLabels[val].col==Color.WHITE){
-                    first.validation = check.validation(first.arrayLabels, val);
-                    if (first.validation) {
+                    validation1 = check.validation(first.arrayLabels, val);
+                    if (validation1) {
                         break;
                     }
                 }
             }
-            if (!first.validation&&!second.validation){
+            if (!validation1&&!validation2){
                 int white = 0;
                 int black = 0;
                 for (int i = 0;i<64;i++){
@@ -284,24 +282,22 @@ public class front extends Thread{
                 if(white==black){
                     JOptionPane.showMessageDialog(second.visual,"Draw: "+black+":"+white);
                 }
-                System.out.println("End White");
                 System.exit(0);
             }
         }
     }
-    public void initalisation(front first,front second){
+    public void initalisation1(front first) {
         // Checking if new move can be made needs to be repositioned - I think both need to be in both first and second
         one check = new one();
-        two check2 = new two();
-        for(int i = 0; i<64; i++){
+        for (int i = 0; i < 64; i++) {
             int finalI1 = i;
-            first.arrayLabels[i].addActionListener(new ActionListener(){
+            first.arrayLabels[i].addActionListener(new ActionListener() {
                 @Override
-                public void actionPerformed(ActionEvent e){
+                public void actionPerformed(ActionEvent e) {
                     Thread t1 = new Thread(new Runnable() {
                         @Override
                         public void run() {
-                            first.place1(first,second,finalI1,check,check2);
+                            first.place1(first, finalI1, check);
                         }
                     });
                     t1.start();
@@ -309,13 +305,20 @@ public class front extends Thread{
                 }
             });
 
+        }
+    }
+    public void initalisation2(front second){
+        // Checking if new move can be made needs to be repositioned - I think both need to be in both first and second
+        two check = new two();
+        for(int i = 0; i<64; i++){
+            int finalI1 = i;
             second.arrayLabels[i].addActionListener(new ActionListener(){
                 @Override
                 public void actionPerformed(ActionEvent e){
                     Thread t3 = new Thread(new Runnable() {
                         @Override
                         public void run() {
-                            second.place2(first,second,finalI1,check,check2);
+                            second.place2(second,finalI1,check);
                         }
                     });
                     t3.start();
@@ -324,8 +327,6 @@ public class front extends Thread{
         }
     }
     public static void main(String args[]){
-        front first = new front("Reversi - white player");
-        front second = new front("Reversi - black player");
         // Is it the players turn?
         int state = 0;
         // Creation of button
@@ -338,10 +339,8 @@ public class front extends Thread{
         second.boxes();
 
 
-        first.initalisation(first,second);
-        second.initalisation(first,second);
-        first.myText = "First";
-        second.myText = "Second";
+        first.initalisation1(first);
+        second.initalisation2(second);
     }
 }
 
